e58b764d2da45ca1527acf500908f49ce616ef97
Pami Ketolainen <pami.ketolainen@gmail.com>
2011-01-23 01:20:07 +0200

    Changelog and setup.py update

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-
 from setuptools import setup, find_packages
 
 setup(
     name = "spotty",
-    version = "0.1",
+    version = open("debian/changelog").readline().split()[1][1:-1],
     author = "Pami Ketolainen",
     author_email = "pami.ketolainen@gmail.com",
-    description = "Spotify notifacation and media key handler",
+    description = "Spotify Linux desktop integration",
     license = "GPL-3",
-    url = "http://to.be.done",
+    url = "https://github.com/keto/spotty",
     packages = find_packages(),
     entry_points = { "console_scripts": [
             "spotty = spotty.core:main",
